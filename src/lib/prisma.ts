b048a198ddcdc68af75f3d0e66c916787ae72b5b
Don Barto Jr <donbartojunior@gmail.com>
2026-03-02 18:01:28 -0500

    update prisma client structure

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,17 @@
 // src/lib/prisma.ts
 import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
+const connectionString = `${process.env.DATABASE_URL}`;
+
 export const prisma =
-  globalForPrisma.prisma;
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter: new PrismaPg({
+      connectionString: connectionString,
+    }),
+  });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;

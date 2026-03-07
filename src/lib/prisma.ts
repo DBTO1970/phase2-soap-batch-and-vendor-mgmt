df1f49c2c08cbf5a86923e437392a16c7f9b848d
Don Barto Jr <donbartojunior@gmail.com>
2026-03-07 11:56:50 -0500

    Fix PrismaClient instantiation

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,8 +7,6 @@ const connectionString = `${process.env.DATABASE_URL}`;
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    datasourceUrl: connectionString,
-  });
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
